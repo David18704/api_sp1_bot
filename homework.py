9d94df95d2b5bdf233cb731d525a1f9d60ed04a3
Давид <dovmont1rambler.ru@yandex.ru>
2021-08-25 13:49:48 +0300

    1 вариант

--- homework.py
+++ homework.py
@@ -2,50 +2,68 @@ import os
 import time
 import requests
 import telegram
+import logging
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
+
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        return f'Работу {homework_name} еще не проверили'
+    homework_statuses = homework.get('status')
+    if homework_statuses == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
-
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(url, headers=headers, params=payload)
+    except requests.exceptions.RequestException as e:
+        raise f'Ошибка при работе с API: {e}'
     return homework_statuses.json()
 
 
+
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID,text=message)
+
 
 
 def main():
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+    current_timestamp = int(time.time()) 
 
     while True:
         try:
-            ...
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            logging.debug('Отслеживание статуса запущено')
+            send_message(			
+			    parse_homework_status(	
+			        get_homeworks(1628000000)['homeworks'][0]	
+			    )	
+			)
+            logging.info('Бот отправил сообщение')	
+            time.sleep(15 * 60)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
+            error_message = f'Бот упал с ошибкой: {e}'	
+            logging.error(error_message)	
+            bot.send_message(chat_id=CHAT_ID, text=error_message)	
+            time.sleep(15)
 
 
 if __name__ == '__main__':
