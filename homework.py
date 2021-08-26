f218a9978051971cba5a98c19f327af39d8d0688
Давид <dovmont1rambler.ru@yandex.ru>
2021-08-26 22:20:41 +0300

    4  вариант

--- homework.py
+++ homework.py
@@ -8,35 +8,51 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+class TGBotException(Exception):
+    pass
+
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     if homework_name is None:
-        return f'Работу {homework_name} еще не проверили'
+        raise TGBotException('Сообщение не содержит обязательных полей')
     homework_statuses = homework.get('status')
+    if homework_statuses is None:
+        raise TGBotException('Сообщение не содержит обязательных полей')
+    if homework_statuses == 'reviewing':
+        verdict = 'Работа взята в ревью.'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
     if homework_statuses == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    if homework_statuses == 'approved':
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    else:
+        raise TGBotException('Значение поля заполнено неверно')
+   
 
 
 def get_homeworks(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
     try:
         homework_statuses = requests.get(url, headers=headers, params=payload)
     except requests.exceptions.RequestException as e:
-        raise f'Ошибка при работе с API: {e}'
-    return homework_statuses.json()
+        raise requests.exceptions.RequestException('Ошибка при работе с API: {e}')
+    answer = homework_statuses.json()
+    if homework_statuses.status_code != 200:
+        raise requests.exceptions.RequestException('Ошибка при работе с сервером')
+    else:
+        return answer
 
 
 def send_message(message):
@@ -44,13 +60,13 @@ def send_message(message):
 
 
 def main():
-
+    current_timestamp = int(time.time())  
     while True:
         try:
             logging.debug('Отслеживание статуса запущено')
             send_message(
                 parse_homework_status(
-                    get_homeworks(1628000000)['homeworks'][0]
+                    get_homeworks(current_timestamp)['homeworks'][0]
                 )
             )
             logging.info('Бот отправил сообщение')
