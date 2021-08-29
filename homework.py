ee7000195e57065bf129286a6aa71433235d9ce3
Давид <dovmont1rambler.ru@yandex.ru>
2021-08-29 12:14:07 +0300

    вариант C1

--- homework.py
+++ homework.py
@@ -27,7 +27,7 @@ def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     homework_statuses = homework.get('status')
     if homework_name is None or homework_statuses is None:
-        raise TGBotException('Сообщение не содержит обязательных полей')   
+        raise TGBotException('Сообщение не содержит обязательных полей')
     if homework_statuses == 'reviewing':
         verdict = 'Работа взята в ревью.'
     elif homework_statuses == 'rejected':
@@ -37,7 +37,7 @@ def parse_homework_status(homework):
     else:
         raise TGBotException('Значение поля заполнено неверно')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-    
+
 
 def get_homeworks(current_timestamp):
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
@@ -60,16 +60,16 @@ def main():
     current_timestamp = int(time.time())
     while True:
         try:
-            logging.debug('Отслеживание статуса запущено')          
+            logging.debug('Отслеживание статуса запущено')
             new_homework = get_homeworks(current_timestamp)
-            current_timestamp = new_homework.get('current_date')           
+            current_timestamp = new_homework.get('current_date')
             if 'homeworks' not in new_homework:
                 raise TGBotException('Список не существует')
             last_homework = new_homework['homeworks']
             if last_homework != []:
                 send_message(parse_homework_status(last_homework[0]))
             else:
-                raise TGBotException('Список пуст')             
+                raise TGBotException('Список пуст')
             logging.info('Бот отправил сообщение')
             time.sleep(15 * 60)
         except Exception as e:
